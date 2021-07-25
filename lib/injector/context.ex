e4262d0deb81bec324cb9c051d7670020a20e675
Adriano Santos <sleipnir@bsd.com.br>
2021-07-24 22:44:16 -0300

    Initial Context API

--- lib/injector/context.ex
+++ lib/injector/context.ex
@@ -1,20 +1,65 @@
 defmodule Injector.Context do
-  defstruct [:bindings]
+  alias Injector.Context
 
-  @type t :: t(Binding.t())
-  @type t(binding) :: Enumerable.t(binding)
+  @type behavior :: module()
+  @type implementation :: module()
+  @type implementations :: list()
+
+  defstruct [:name, :bindings]
+
+  @type t() :: %__MODULE__{
+          name: atom(),
+          bindings: list()
+        }
 
   defmodule Binding do
+    @type behavior :: module()
+
     defstruct [
       :behavior,
-      :definitions,
-      :default
+      :definitions
     ]
 
     @type t() :: %__MODULE__{
-            behavior: module(),
-            definitions: list(),
-            default: module()
+            behavior: behavior(),
+            definitions: list()
           }
+  end
+
+  defmodule BindingDefinition do
+    defstruct [:module, :alias, :default]
+
+    @type t() :: %__MODULE__{
+            module: module(),
+            alias: atom() | nil,
+            default: boolean() | false
+          }
+  end
+
+  @spec from_config(atom()) :: :ok | {:error, :not_found}
+  def from_config(_otp_app) do
+    :ok
+  end
+
+  @spec from_definition(Context.t()) :: :ok
+  def from_definition(context) do
+    case Agent.start_link(fn -> %{} end, name: context.name) do
+      {:ok, _pid} ->
+        Agent.update(context.name, fn state -> Map.merge(state, %{bindings: context.bindings}) end)
+
+      {:error, {:already_started, _pid}} ->
+        nil
+    end
+
+    :ok
+  end
+
+  @spec binding(String.t(), behavior()) :: implementation()
+  def binding(_name, _behavior) do
+  end
+
+  @spec bindings(String.t(), behavior()) :: implementations()
+  def bindings(_name, _behavior) do
+    []
   end
 end
