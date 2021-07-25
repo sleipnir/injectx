838d80d6b75c3be248a6e1ea8ce9a2107caae1da
Adriano Santos <sleipnir@bsd.com.br>
2021-07-24 21:14:30 -0300

    initial implementation of injector

--- lib/injector.ex
+++ lib/injector.ex
@@ -2,4 +2,39 @@ defmodule Injector do
   @moduledoc """
   `Injector` is a entrypoint to CDI.
   """
+
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      alias Injector
+      require Injector
+
+      @options opts
+
+      Module.register_attribute(__MODULE__, :inject, accumulate: true)
+    end
+  end
+
+  def __on_definition__(env, _kind, _name, _args, _guards, _body) do
+    resolve_injections(env.module)
+  end
+
+  defp resolve_injections(module) do
+    injection_points = Module.get_attribute(module, :inject)
+    Module.put_attribute(module, :imports, resolve_bindings(injection_points))
+  end
+
+  defp resolve_bindings(injection_points) do
+    # TODO resolve points
+    Enum.map(injection_points, fn point -> nil end)
+  end
+
+  defmacro __before_compile__(env) do
+    imports = Module.get_attribute(env.module, :imports)
+
+    for import <- imports do
+      quote bind_quoted: [import: import] do
+        alias import
+      end
+    end
+  end
 end
--- lib/injector/context.ex
+++ lib/injector/context.ex
@@ -1,2 +1,20 @@
 defmodule Injector.Context do
+  defstruct [:bindings]
+
+  @type t :: t(Binding.t())
+  @type t(binding) :: Enumerable.t(binding)
+
+  defmodule Binding do
+    defstruct [
+      :behavior,
+      :definitions,
+      :default
+    ]
+
+    @type t() :: %__MODULE__{
+            behavior: module(),
+            definitions: list(),
+            default: module()
+          }
+  end
 end
